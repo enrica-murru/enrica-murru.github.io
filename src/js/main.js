44526b47c67644310873aa825497d19e1afd5822
enrica-murru <enicacasa@hotmail.it>
2014-04-01 18:39:26 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+/* global jQuery, setTimeout */
+(function ($, setTimeout) {
+    'use strict';
 
-    <title>Theme Template for Bootstrap</title>
+	$(function () {
 
-    <!-- Bootstrap core CSS -->
-    <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap theme -->
-    <link href="http://getbootstrap.com/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+		var origin = "src/templates/";
+		var loadContent = function (selector, url) {
+			$.ajax({
+				url: origin + url
+			}).done(function(content) {
+				$(selector).html(content);
+			});
+		};
+		var onHashChange = function () {
+			setTimeout(function () {
+				var url = window.location.hash || "#home.html";
 
-    <!-- Custom styles for this template -->
-    <link href="http://getbootstrap.com/examples/theme/theme.css" rel="stylesheet">
+				url = url.substring(1, url.length);
+				loadContent('#main', url);
+			}, 0);
+		};
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body role="document">
-
-    <!-- Fixed navbar -->
-    <div id="navbar" class="navbar navbar-inverse navbar-fixed-top" role="navigation"></div>
-
-    <div id="main" class="container theme-showcase" role="main"></div> <!-- /container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-    <script src="http://getbootstrap.com/assets/js/docs.min.js"></script>
-    <script src="src/js/main.js"></script>
-  </body>
-</html>
+		loadContent('#navbar', 'navbar.html');
+		loadContent('#main', 'home.html');
+		$('#navbar.navbar').on('click', onHashChange);
+	});
+})(jQuery, setTimeout);
